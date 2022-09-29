664d8d07f47d657fd49e316f3942bf8f20313869
Joshua <mwalejoshua89@gmail.com>
2022-09-29 12:42:02 +0300

    Unfinished

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,26 @@
 class Artist < ApplicationRecord
-    has_many :songs, dependent: :destroy
+    has_many :tracks, dependent: :destroy
+
+    # validates :name, {
+    #     presence: true,
+    #     length: {
+    #         in: 3..20
+    #     },
+    #     numericality: false,
+    #     uniqueness: true
+
+    # }
+
+    validate :name_length
+    validate :name_domain
+
+    def name length
+        unless name.length >= 3 && name.length <= 20
+            errors.add(:name, "Name must be at least 3 characters long")
+        end
+    end
+
+    def name_domain
+        unless name.match?(/moringaschool.com/)
+            errors.add(:name, "Name must be from moringaschool")
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,40 +1,45 @@
-puts "Creating Artists ..."
+# puts "Creating Artists ..."
+
+# Artist.create({
+#     name: "Trio Mio",
+#     age: 17
+# })
+
+# Artist.create({
+#     name: "Nandy",
+#     age: 29
+# })
+
+# Artist.create({
+#     name: "Mejja",
+#     age: 36
+# })
+
+# puts "Thats some proper lineup!"
+
+# puts "Some songs added here..."
+
+# Track.create({
+#     title: "Cheza kama wewe",
+#     duration: 4,
+#     artist_id: trio
+# })
+
+# Track.create({
+#     title: "Kanairo Dating",
+#     duration: 3,
+#     artist_id: mejja
+# })
+
+# Track.create({
+#     title: "Nimekuzoea",
+#     duration: 4,
+#     artist_id: nandy
+# })
+
+# puts "Your songs are here!"
 
 Artist.create({
-    name: "Trio Mio",
-    age: 17
+    name: "Wee",
+    age: 18
 })
-
-Artist.create({
-    name: "Nandy",
-    age: 29
-})
-
-Artist.create({
-    name: "Mejja",
-    age: 36
-})
-
-puts "Thats some proper lineup!"
-
-puts "Some songs added here..."
-
-Song.create({
-    title: "Cheza kama wewe",
-    duration: 4,
-    artist: 1
-})
-
-Song.create({
-    title: "Kanairo Dating",
-    duration: 3,
-    artist: 3
-})
-
-Song.create({
-    title: "Nimekuzoea",
-    duration: 4,
-    artist: 2
-})
-
-puts "Your songs are here!"
